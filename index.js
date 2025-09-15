783a9ad6eaf635f02b82bcb9dcc30ea94172822d
ariekas <ariariekas@gmail.com>
2025-09-15 13:56:56 +0700

    feat: init new project and create function menuUtama on index.js containing output list menu

---
+++ index.js
@@ -0,0 +1,9 @@
+async function menuUtama() {
+    console.log("Selamat datang di Pertamina, Silahkan pilih menu: ");
+    console.log("1. Pilih bahan bakar");
+    console.log("2. Lihat History");
+    console.log("3. EXIT");
+}
+
+
+menuUtama();
