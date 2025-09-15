3dbddf10274049165ca39ea2df2ed470b6a2f877
ariekas <ariariekas@gmail.com>
2025-09-15 14:02:05 +0700

    feat: creating question function implementasi package readline and made so that it can be used anywhere, implementasio input question and exiting program in index.js

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-async function menuUtama() {
-    console.log("Selamat datang di Pertamina, Silahkan pilih menu: ");
-    console.log("1. Pilih bahan bakar");
-    console.log("2. Lihat History");
-    console.log("3. EXIT");
-}
+const { question, exiting } = require("./lib/questions/index");
 
+async function menuUtama() {
+  console.log("Selamat datang di Pertamina, Silahkan pilih menu: ");
+  console.log("1. Pilih bahan bakar");
+  console.log("2. Lihat History");
+  console.log("3. EXIT");
+  const input = await question("Silahkan pilih menu: (1-3)");
+
+  switch (input) {
+    case "1":
+      console.log("menu pilih bahan bakar");
+      break;
+    case "2":
+      console.clear();
+      console.log("menu History");
+      break;
+    case "3":
+      console.log("Silahkan datang kembali!");
+      console.clear();
+      exiting();
+      break;
+    default:
+      console.log(
+        await question(
+          " ⚠️ Input yang dimasukan salah, Tekan 'Enter' untuk kembali ke manu"
+        )
+      );
+      console.clear();
+      await menuUtama();
+      break;
+  }
+}
 
 menuUtama();
